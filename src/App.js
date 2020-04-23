13729fdf9512642a3d36d198fa42dfb20f303f01
josephs <josef2011@gmail.com>
2020-04-23 17:05:39 +0300

    Ãdding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component'
+// import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [monsters, monsterSet] = useState([]);
+    const [searchField, searchSet] = useState('');
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                monsterSet(users);
+            });
+    }, []);
+    const onSearchChange = event => {
+        searchSet(event.target.value)
+    };
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+        <div className="App">
+            <h1>Monsters Rolodex</h1>
+            <SearchBox onSearchChange={onSearchChange}/>
+            <CardList monsters={filteredMonsters} />
+        </div>
+    );
 }
 
 export default App;
