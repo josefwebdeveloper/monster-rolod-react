5d9f054aef5af598a2bdfaa0e3f71e7923eac2d0
josephs <josef2011@gmail.com>
2020-04-23 17:03:30 +0300

    Updates

---
+++ precache-manifest.57f4b5058a66b0ea0b92123c91a28741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5663a49bad887bbf4ef9e2548de00a29",
+    "url": "/monster-rolod-react/index.html"
+  },
+  {
+    "revision": "3e6d04c9cd94734bc8c9",
+    "url": "/monster-rolod-react/static/css/main.21e3d468.chunk.css"
+  },
+  {
+    "revision": "f97cd25035ec2e1b2175",
+    "url": "/monster-rolod-react/static/js/2.fc788891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolod-react/static/js/2.fc788891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6d04c9cd94734bc8c9",
+    "url": "/monster-rolod-react/static/js/main.59052358.chunk.js"
+  },
+  {
+    "revision": "82a1bc7a108fb3bc74e9",
+    "url": "/monster-rolod-react/static/js/runtime-main.d9833ab3.js"
+  }
+]);
